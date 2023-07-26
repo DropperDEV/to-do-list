d146cfa3763323398146a356bd6e07f9f2381059
dropperDEV <jamescosta4747@Gmail.com>
2023-07-26 15:40:43 -0300

    input text finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,95 @@
+import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import "./App.css";
 
 export default function App() {
+  const [toDos, setToDo] = useState([]);
+  function handleAddItems(newToDo) {
+    setToDo((toDos) => [...toDos, newToDo]);
+  }
+
   return (
     <div className="main">
-      <div className="title-wrapper">
-        <h1>THINGS TO DO</h1>
-      </div>
-
-      <input type="text" className="input-text" placeholder="Add New" />
-      <div className="wrapper-checkbox">
-        <div className="checkbox">
-          <input type="checkbox" name="" id="" />
-          <h2>Huhdsauhduahsdha</h2>
-        </div>
-        <div className="checkbox">
-          <input type="checkbox" name="" id="" />
-          <h2>Huhdsauhduahsdha</h2>
-        </div>
-        <div className="checkbox">
-          <input type="checkbox" name="" id="" />
-          <h2>Huhdsauhduahsdha</h2>
-        </div>
-      </div>
-      <div className="footer">
-        <div className="footer-element">
-          <span>➕</span>
-          <span>🔍</span> | <p>3 items left</p>
-        </div>
-        <div className="footer-element">
-          <p>All</p>
-          <p>Active</p>
-          <p>Completed</p>
-        </div>
-      </div>
+      <Title>THINGS TO DO </Title>
+      <InputText onAddItems={handleAddItems} />
+      <ListToDo toDos={toDos} onToDo={setToDo} />
+      <Footer />
     </div>
   );
+}
+
+function Title({ children }) {
+  return (
+    <div className="title-wrapper">
+      <h1>{children}</h1>
+    </div>
+  );
+}
+
+function InputText({ onAddItems }) {
+  const [name, setName] = useState("");
+  function handleAddToDo(e) {
+    e.preventDefault();
+    const newToDo = { id: uuidv4(), name, state: 0 };
+    onAddItems(newToDo);
+    setName("");
+  }
+  return (
+    <form action="" onSubmit={handleAddToDo} className="inputText-form">
+      <input
+        type="text"
+        className="input-text"
+        placeholder="Add New"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function ListToDo({ toDos, onToDo }) {
+  return (
+    <div className="wrapper-checkbox">
+      {toDos.map((todo) => (
+        <ToDo Todo={todo} key={todo.id} onToDo={onToDo} />
+      ))}
+    </div>
+  );
+}
+
+function ToDo({ Todo, onToDo }) {
+  function handleToggleClick() {
+    const updatedTodo = { ...Todo, state: 1 };
+    onToDo(updatedTodo);
+  }
+  return (
+    <div className={Todo.state ? "checkbox selected" : "checkbox"}>
+      <input
+        type="checkbox"
+        checked={Todo.state}
+        onChange={handleToggleClick}
+      />
+      <h2>{Todo.name}</h2>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="footer">
+      <BottomOptions>
+        <span>➕</span>
+        <span>🔍</span> <p>3 items left</p>
+      </BottomOptions>
+      <BottomOptions>
+        <p>All</p>
+        <p>Active</p>
+        <p>Completed</p>
+      </BottomOptions>
+    </div>
+  );
+}
+
+function BottomOptions({ children }) {
+  return <div className="footer-element">{children}</div>;
 }
